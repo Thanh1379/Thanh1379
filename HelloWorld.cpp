f85b0d5e745fdf31f433c58e097129d87ddf055a
NDV THANH <vienthanh136@gmail.com>
2022-05-05 20:03:26 +0700

    test debug

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,11 +1,23 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+void inNhiPhan(int n);
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "And Happy New Year!!!!!!!!!!!!!!!!";
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    inNhiPhan(n);
     
     return 0;
+}
+
+void inNhiPhan(int n)
+{
+    if (n > 0)
+    {
+        inNhiPhan(n / 2);
+        printf("%d", n % 2);
+    }
 }
